30456e4ff0f0dff6b3f49329102f039dd67790e2
Mukul <mukul_hs@live.com>
2018-07-12 17:24:01 +0530

    small changes

--- FireTrip/app/src/main/java/com/example/mukul_pc/firetrip/Dashboard.java
+++ FireTrip/app/src/main/java/com/example/mukul_pc/firetrip/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.mukul_pc.firetrip;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +10,17 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Dashboard extends AppCompatActivity {
 
     private Button log,updt;
     private FirebaseAuth mAuth;
-    private TextView user;
+    private TextView user,userN,userA;
     String user_id,name;
     int age;
     DatabaseReference dbref;
@@ -27,9 +31,7 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         mAuth = FirebaseAuth.getInstance();
-
         dbref = FirebaseDatabase.getInstance().getReference();
-        Log.e("Reference : ",dbref.toString());
 
         user_id = mAuth.getCurrentUser().getUid();
         user = (TextView)findViewById(R.id.uid);
@@ -37,14 +39,34 @@ public class Dashboard extends AppCompatActivity {
         String names[] = (mAuth.getCurrentUser().getEmail()).split("@");
         name = names[0];
         age = 21;
+        userN = (TextView)findViewById(R.id.userName);
+        userA = (TextView)findViewById(R.id.userAge);
 
         log = (Button)findViewById(R.id.logoutBtn);
         updt = (Button)findViewById(R.id.dbAdd);
 
+
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String n = (dataSnapshot.child("Users").child(user_id).child("name").getValue()).toString();
+                String a = (dataSnapshot.child("Users").child(user_id).child("age").getValue()).toString();
+                userN.setText(n);
+                userA.setText(a);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        Log.e("Reference : ",dbref.toString());
+
         log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
+                mAuth.signOut();
                 startActivity(new Intent(Dashboard.this,Fire1.class));
             }
         });
